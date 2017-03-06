32473dff5f34319de705501bc72af02f4807954d
gebergman <georgi.bergman@gmail.com>
2017-03-06 12:56:14 +0200

    fixing for nested media queries

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ Sprites.prototype.writeStyles = function () {
 
       if (this.retina) {
         retinaQuery = util.format(
-          '  @media all and (-webkit-min-device-pixel-ratio: 1.5),\n' +
+          '  @media (-webkit-min-device-pixel-ratio: 1.5),\n' +
           '  (min--moz-device-pixel-ratio: 1.5),\n' +
           '  (-o-min-device-pixel-ratio: 3/2), (min-device-pixel-ratio: 1.5) {\n' +
           '    background-image: url("%s");\n' +
